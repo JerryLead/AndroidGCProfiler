4ac3c00928d5f1cfccf60d29407846b14911ba96
Lijie Xu <xulijie09@qq.com>
2018-08-11 15:27:19 +0800

    Add basic log parser

---
+++ src/main/python/statistics/GCActivities.py
@@ -0,0 +1,57 @@
+from statistics.GCEvent import GCEvent
+import time as Time
+
+
+class GCActivities:
+
+
+
+    # line = Background partial concurrent mark sweep GC freed 22037(4MB) AllocSpace objects, 9(79MB) LOS objects, 7% free, 190MB/206MB, paused 50.117ms total 712.383ms
+    def parseGCEvent(self, line):
+
+
+        timeStr = line[0: line.find(' ', line.find(' ') + 1)]
+        time = Time.strptime(timeStr,"%m-%d %H:%M:%S")
+        gcCause = line[line.find('art:') + 5: line.find('freed') - 1]
+        freedObjNum = int(line[line.find('freed') + 6: line.find('(')])
+        freedObjSize = int(line[line.find('(') + 1: line.find('MB)')])
+        largeObjNum = int(line[line.find(',') + 2: line.rfind('(')])
+        largeObjSize = int(line[line.rfind('(') + 1: line.rfind('MB)')])
+        used = int(line[line.rfind('free,') + 6: line.rfind('MB/')])
+        allocated = int(line[line.rfind('/') + 1: line.rfind('MB')])
+        gcPauseStr = line[line.rfind('paused') + 7: line.rfind(' total')]
+
+        if(gcPauseStr.endswith('us')):
+            gcPauseTime = float(gcPauseStr[0: gcPauseStr.rfind('us')])
+            gcPauseTime = gcPauseTime / 1000
+        elif(gcPauseStr.endswith('ms')):
+            gcPauseTime = float(gcPauseStr[0: gcPauseStr.rfind('ms')])
+        elif(gcPauseStr.endswith('s')):
+            gcPauseTime = float(gcPauseStr[0: gcPauseStr.rfind('s')]) * 1000
+
+
+        gcTotalStr = line[line.rfind(' ') + 1:]
+        if(gcTotalStr.endswith('us')):
+            gcTotalTime = float(gcTotalStr[0: gcTotalStr.rfind('us')])
+            gcTotalTime = gcTotalTime / 1000
+        elif(gcTotalStr.endswith('ms')):
+            gcTotalTime = float(gcTotalStr[0: gcTotalStr.rfind('ms')])
+        elif(gcTotalStr.endswith('s')):
+            gcTotalTime = float(gcTotalStr[0: gcTotalStr.rfind('s')]) * 1000
+
+
+        gcEvent = GCEvent(time, gcCause, freedObjNum, freedObjSize, largeObjNum, largeObjSize,
+                          used, allocated, gcPauseTime, gcTotalTime)
+        gcEvent.println()
+
+
+
+
+
+if __name__ == '__main__':
+
+    gcActivities = GCActivities()
+    line = "06-16 12:07:14.971 27469-27479/com.library.example.nin I/art: Background partial concurrent mark sweep GC freed 22037(4MB) AllocSpace objects, 9(79MB) LOS objects, " \
+           "7% free, 190MB/206MB, paused 50.117ms total 712.383ms"
+
+    gcActivities.parseGCEvent(line)
---
+++ src/main/python/statistics/GCEvent.py
@@ -0,0 +1,51 @@
+class GCEvent:
+
+    def __init__(self, time, gcCause, freedObjNum, freedObjSize, largeObjNum, largeObjSize,
+                 used, allocated, gcPauseTime, gcTotalTime):
+        self.time = time
+        self.gcCause = gcCause
+        self.freedObjNum = freedObjNum
+        self.freedObjSize = freedObjSize #MB
+        self.largeObjNum = largeObjNum
+        self.largeObjSize = largeObjSize #MB
+        self.used = used #MB
+        self.allocated = allocated #MB
+        self.gcPauseTime = gcPauseTime #ms
+        self.gcTotalTime = gcTotalTime #ms
+
+    def getTime(self):
+        return self.time
+
+    def getGCCause(self):
+        return self.gcCause
+
+    def getFreedObjNum(self):
+        return self.freedObjNum
+
+    def getFreedObjSize(self):
+        return self.freedObjSize
+
+    def getLargeObjNum(self):
+        return self.largeObjNum
+
+    def getLargeObjSize(self):
+        return self.largeObjSize
+
+    def getUsed(self):
+        return self.used
+
+    def getAllocated(self):
+        return self.allocated
+
+    def getGCPauseTime(self):
+        return self.gcPauseTime
+
+    def getGCTotalTime(self):
+        return self.gcTotalTime
+
+    def println(self):
+        print(self.time + '|' + self.gcCause + '|' + str(self.freedObjNum) + '|'
+              + str(self.freedObjSize) + 'MB|' + str(self.largeObjNum) + '|'
+              + str(self.largeObjSize) + 'MB|' + str(self.used) + 'MB|' + str(self.allocated) + 'MB|'
+              + str(self.gcPauseTime) + 'ms|' + str(self.gcTotalTime)) + 'ms'
+
